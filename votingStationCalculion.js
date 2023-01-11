a3712485407e80fb7f872b6390437e150ae39043
Ulrich-Romeo-Ngniaba <cheriepassy2011@gmail.com>
2023-01-11 21:15:31 +0100

    voting station calculation function

---
+++ votingStationCalculion.js
@@ -0,0 +1,14 @@
+const chooseStations = function(stations){
+  let goodStations = [];
+
+  for (const station of stations) {
+    if (station[1] >= 20) {
+      if (station[2] === "school" || station[2] === "community centre") {
+        goodStations.push(station[0]);
+      }
+    }
+  }
+
+  return goodStations;
+}
+
